a70734afcc9b548b61089a0ccae0dd0db992bb8a
haythemlp <haythamov1993@gmail.com>
2019-04-12 22:26:23 +0100

    rename file

--- src/main/java/com/haythemlp/springboot/Application.java
+++ src/main/java/com/haythemlp/springboot/Application.java
@@ -1,15 +1,11 @@
 package com.haythemlp.springboot;
 
 
-import com.haythemlp.springboot.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.sql.DataSource;
-
 @SpringBootApplication
-public class ToDoList {
+public class Application {
 
 
 
@@ -17,6 +13,6 @@ public class ToDoList {
     public static void main(String[] args)
 
     {
-        SpringApplication.run(ToDoList.class,args);
+        SpringApplication.run(Application.class,args);
     }
 }
